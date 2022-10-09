3880e0fea5918f9fb8c2185860294f3e3d796880
IseEkko <71496666+IseEkko@users.noreply.github.com>
2022-10-09 11:05:44 +0800

    add_负载均衡_20221009

--- user_srv/main.go
+++ user_srv/main.go
@@ -5,24 +5,33 @@ import (
 	"Completed_Server/user_srv/handler"
 	"Completed_Server/user_srv/initialize"
 	"Completed_Server/user_srv/proto"
+	"Completed_Server/user_srv/utils"
 	"flag"
 	"fmt"
 	"github.com/hashicorp/consul/api"
+	"github.com/satori/go.uuid"
 	"go.uber.org/zap"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/health"
 	"google.golang.org/grpc/health/grpc_health_v1"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 	IP := flag.String("ip", "0.0.0.0", "ip地址")
-	Port := flag.Int("port", 50051, "端口号")
+	Port := flag.Int("port", 0, "端口号")
 	//初始化
 	initialize.InitLogger()
 	initialize.InitConfig()
 	initialize.InitDB()
 	flag.Parse()
+	//动态的获取监听端口
+	if *Port == 0 {
+		*Port, _ = utils.GetFrePort()
+	}
 	zap.S().Info("ip:", *IP)
 	zap.S().Info("port:", *Port)
 	server := grpc.NewServer()
@@ -45,7 +54,7 @@ func main() {
 		panic(err)
 	}
 	check := &api.AgentServiceCheck{
-		GRPC:                           "192.168.6.119:50051",
+		GRPC:                           fmt.Sprintf("192.168.6.119:%d", *Port),
 		Timeout:                        "5s",
 		Interval:                       "5s",
 		DeregisterCriticalServiceAfter: "15s",
@@ -53,7 +62,8 @@ func main() {
 	//生成注册对象
 	registration := new(api.AgentServiceRegistration)
 	registration.Name = global.SerConfig.Name
-	registration.ID = global.SerConfig.Name
+	serviceId := fmt.Sprintf("%s", uuid.NewV4())
+	registration.ID = serviceId
 	registration.Port = *Port
 	registration.Tags = []string{"imooc", "bobby", "user", "srv"}
 	registration.Address = "192.168.6.119"
@@ -63,8 +73,24 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	err = server.Serve(lis)
-	if err != nil {
-		panic("failed to start grpc:" + err.Error())
+	/**
+	这里进行了阻塞，也就是说这里一直阻塞着，就不会拿到信号量，那么就一直不会退出
+	解决的办法：使用异步操作
+	*/
+	go func() {
+		err = server.Serve(lis)
+		if err != nil {
+			panic("failed to start grpc:" + err.Error())
+		}
+	}()
+
+	//优雅的退出服务
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	if err = client.Agent().ServiceDeregister(serviceId); err != nil {
+		zap.S().Info("注销失败")
 	}
+	zap.S().Info("注销成功")
+
 }
